d9c735f27f01e631e0ffebd533b43640997cb1c5
涂治昌 <617926532@qq.com>
2017-06-07 22:32:23 +0800

    歌单列表

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -12,7 +12,17 @@
       </div>
       <div class="recommend-list">
         <h1>热门歌单推荐</h1>
-        <ul></ul>
+        <ul>
+          <li v-for="item in discList" class="item">
+            <div class="icon">
+              <img width="60" height="60" :src="item.imgurl">
+            </div>
+            <div class="text">
+              <h2 class="name" v-html="item.creator.name"></h2>
+              <p class="desc" v-html="item.dissname"></p>
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
@@ -20,22 +30,31 @@
 
 <script>
   import Slider from 'base/slider/slider'
-  import { getRecommend } from 'api/recommend'
+  import { getRecommend, getDiscList } from 'api/recommend'
   import { ERR_OK } from 'api/config'
   export default {
     data () {
       return {
-        recommends: []
+        recommends: [],
+        discList: []
       }
     },
     created () {
       this._getRecommend()
+      this._getDiscList()
     },
     methods: {
       _getRecommend () {
         getRecommend().then((res) => {
           if (res.code === ERR_OK) {
             this.recommends = res.data.slider
+          }
+        })
+      },
+      _getDiscList () {
+        getDiscList().then((res) => {
+          if (res.code === ERR_OK) {
+            this.discList = res.data.list
           }
         })
       }
@@ -48,6 +67,7 @@
 
 <style lang="scss" scoped>
   @import "../../common/style/variable";
+
   .recommend {
     position: fixed;
     width: 100%;
@@ -67,7 +87,47 @@
         text-align: center;
         font-size: $font-size-medium;
         color: $color-theme;
+        .list-title {
+          height: 65px;
+          line-height: 65px;
+          text-align: center;
+          font-size: $font-size-medium;
+          color: $color-theme;
+        }
+        .item {
+          display: flex;
+          box-sizing: border-box;
+          align-items: center;
+          padding: 0 20px 20px 20px;
+          .icon {
+            flex: 0 0 60px;
+            width: 60px;
+            padding-right: 20px
+          }
+          .text {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            flex: 1;
+            line-height: 20px;
+            overflow: hidden;
+            font-size: $font-size-medium;
+            .name {
+              margin-bottom: 10px;
+              color: $color-text
+            }
+            .desc {
+              color: $color-text-d
+            }
+          }
+        }
       }
+    }
+    .loading-container {
+      position: absolute;
+      width: 100%;
+      top: 50%;
+      transform: translateY(-50%)
     }
   }
 </style>
